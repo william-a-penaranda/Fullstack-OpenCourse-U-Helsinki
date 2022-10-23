1e368ce4c27252310dde5c80e857ff019d2f197d
William <williamapenaranda@gmail.com>
2022-10-23 20:34:26 +0200

    renamed folders

--- section-2/the phonebook/src/App.js
+++ section-2/the phonebook/src/App.js
@@ -1,19 +1,25 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
 import SearchFilter from './components/SearchFilter';
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-123456', id: 1 },
-      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState("");
   const [searchWord, setSearchWord] = useState("");
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log("promise fulfilled");
+        setPersons(response.data);
+      })
+  }, [])
+
 
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
