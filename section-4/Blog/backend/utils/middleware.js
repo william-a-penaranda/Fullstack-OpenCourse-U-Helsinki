2141e5a76bf41c215ca21e675d4ea9e6e602057d
William <williamapenaranda@gmail.com>
2022-11-07 12:00:03 +0100

    added error JWT errors to error handler

--- section-4/Blog/backend/utils/middleware.js
+++ section-4/Blog/backend/utils/middleware.js
@@ -20,6 +20,10 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted data' });
   } else if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).send({ error: 'missing token' });
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).send({ error: 'expired token' });
   }
 
   next(error);
